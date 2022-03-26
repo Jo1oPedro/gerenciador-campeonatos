5500d5f19947c6639572d92147aa6a601894679c
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-03-26 02:49:07 -0300

    Adiciona model e factory para jogador

---
+++ database/factories/JogadorFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JogadorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->firstName(),
+            'idade' => $this->faker->numberBetween($min = 1,$max = 115),
+            'time' => $this->faker->firstName(),
+            'nacionalidade' => $this->faker->city(),
+        ];
+    }
+}
